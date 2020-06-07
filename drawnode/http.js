71593b221bdd80d55a46f91a85d1ce588fa9afb1
unknown <elephent.blue@gmail.com>
2020-06-07 08:57:36 -0700

    hello2

--- drawnode/http.js
+++
@@ -1,4 +0,0 @@
-require('http').createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hei, Trondheim!');
-}).listen(1337);
--- drawnode/tcp.js
+++
@@ -1,4 +0,0 @@
-require('net').createServer(function(socket) {
-  socket.write('Here be echo.');
-  socket.pipe(socket);
-}).listen(1337);
